809646cf15f9fc5ee0ebe9e1aee5cbf2db0e80d8
Mario Limonciello <mario.limonciello@amd.com>
2026-04-08 10:24:38 -0500

    Remove all files

--- format-code.sh
+++
@@ -1,8 +0,0 @@
-for f in src/*.cpp src/*.h src/*.hpp src/vocab/*.h src/vocab/*.cpp examples/cli/*.cpp examples/common/*.hpp examples/cli/*.h examples/server/*.cpp; do
-  [[ "$f" == vocab* ]] && continue
-  echo "formatting '$f'"
-  # if [ "$f" != "stable-diffusion.h" ]; then
-  #   clang-tidy -fix -p build_linux/ "$f"
-  # fi
-  clang-format -style=file -i "$f"
-done
